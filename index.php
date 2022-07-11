96d68e310bc2a355708f97471a1e97e1fc4de22c
EnricoPiombini <piombini.enrico@gmail.com>
2022-07-11 17:30:18 +0200

    navbar mancante

--- index.php
+++ index.php
@@ -4,7 +4,9 @@ $db = [
 
     [
         "text" => [
+
             "question" => "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
+
         ],
         "paragraph" => [
             "La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.",
@@ -19,6 +21,7 @@ $db = [
         "text" => [
 
             "question" => "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?",
+
         ],
         "paragraph" => [
             "Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.",
@@ -32,6 +35,7 @@ $db = [
         "text" => [
 
             "question" => "Perché il mio account è associato a un paese?",
+
         ],
         "paragraph" => [
             "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:",
@@ -44,6 +48,7 @@ $db = [
         "text" => [
 
             "question" => "Perché il mio account è associato a un paese?",
+
         ],
         "paragraph" => [
             "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:",
@@ -55,6 +60,7 @@ $db = [
         "text" => [
 
             "question" => "Stabilire il paese associato al tuo account",
+
         ],
         "paragraph" => [
             "Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno.",
@@ -67,6 +73,7 @@ $db = [
         "text" => [
 
             "question" => "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
+
         ],
         "paragraph" => [
             "I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni.",
@@ -77,6 +84,7 @@ $db = [
         "text" => [
 
             "question" => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
+
         ],
         "paragraph" => [
             "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio.",
@@ -94,14 +102,16 @@ $db = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <title>Google Faq</title>
 </head>
 
 <body>
-    <div class="container">
+    <div class="container px-5">
 
         <?php foreach ($db as $faq) { ?>
-            <?php echo $faq["text"]["question"] ?>
+            <h2><?php echo $faq["text"]["question"] ?></h2>
 
             <p>
                 <?php foreach ($faq["paragraph"] as $content) {
